4b4ef302962595e58cf4de179d4b3fd855b12fb7
DrNour <43105891+DrNour@users.noreply.github.com>
2025-09-06 09:53:46 +0300

    Delete instructor_dashboard.py

--- instructor_dashboard.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def run():
-    st.title("Instructor Dashboard")
-    st.write("Welcome Instructor! Monitor student performance and analytics here.")
-    # Instructor analytics and charts will go here later
